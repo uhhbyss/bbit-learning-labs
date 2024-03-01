22c80743889055f6a5227cc453106f2328d5fd62
Joshx27 <joshuajhill2002@gmail.com>
2024-03-01 11:31:16 -0600

    last commit

--- Tech-Lab-On-Campus/Topic-Exchange/consume.py
+++ Tech-Lab-On-Campus/Topic-Exchange/consume.py
@@ -23,6 +23,8 @@ def main(sector: str, queueName: str) -> None:
     #
     #                       WRITE CODE HERE!!!
     #
+
+    bindingKey = sector + queueName
     
     consumer = mqConsumer(binding_key=bindingKey,exchange_name="Tech Lab Topic Exchange",queue_name=queueName)    
     consumer.startConsuming()
@@ -36,4 +38,13 @@ if __name__ == "__main__":
     #                       WRITE CODE HERE!!!
     #
 
-    sys.exit(main(sector,queue))
+    sector = sys.argv[1]
+    queueName = sys.argv[2]
+
+    print(sector, queueName)
+
+
+
+    
+
+    sys.exit(main(sector,queueName))
